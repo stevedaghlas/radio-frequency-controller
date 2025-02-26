8f425813a7fcfadbaca07e4b86df073c39059e97
Steve Daghlas <steve.daghlas@outlook.com>
2025-02-26 09:42:49 +0100

    WIP:frequencies before decimal working

---
+++ RadioFrequencyController/MainForm.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace RadioFrequencyController
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.lblFrequencyDisplay = new System.Windows.Forms.Label();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Font = new System.Drawing.Font("Audiowide", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(19, 53);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(91, 48);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // lblFrequencyDisplay
+            // 
+            this.lblFrequencyDisplay.AutoSize = true;
+            this.lblFrequencyDisplay.Font = new System.Drawing.Font("Audiowide", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFrequencyDisplay.Location = new System.Drawing.Point(70, 9);
+            this.lblFrequencyDisplay.Name = "lblFrequencyDisplay";
+            this.lblFrequencyDisplay.Size = new System.Drawing.Size(175, 41);
+            this.lblFrequencyDisplay.TabIndex = 3;
+            this.lblFrequencyDisplay.Text = "122.800";
+            this.lblFrequencyDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn2
+            // 
+            this.btn2.Font = new System.Drawing.Font("Audiowide", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(112, 53);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(91, 48);
+            this.btn2.TabIndex = 4;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Font = new System.Drawing.Font("Audiowide", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(204, 53);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(91, 48);
+            this.btn3.TabIndex = 5;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Font = new System.Drawing.Font("Audiowide", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(204, 107);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(91, 48);
+            this.btn6.TabIndex = 8;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Font = new System.Drawing.Font("Audiowide", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(112, 107);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(91, 48);
+            this.btn5.TabIndex = 7;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Font = new System.Drawing.Font("Audiowide", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(19, 107);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(91, 48);
+            this.btn4.TabIndex = 6;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Font = new System.Drawing.Font("Audiowide", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(204, 161);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(91, 48);
+            this.btn9.TabIndex = 11;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Font = new System.Drawing.Font("Audiowide", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(112, 161);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(91, 48);
+            this.btn8.TabIndex = 10;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Font = new System.Drawing.Font("Audiowide", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(19, 161);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(91, 48);
+            this.btn7.TabIndex = 9;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Font = new System.Drawing.Font("Audiowide", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0.Location = new System.Drawing.Point(112, 215);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(91, 48);
+            this.btn0.TabIndex = 12;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Audiowide", 12F, System.Drawing.FontStyle.Bold);
+            this.btnReset.Location = new System.Drawing.Point(19, 215);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(91, 48);
+            this.btnReset.TabIndex = 13;
+            this.btnReset.Text = "RESET";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Font = new System.Drawing.Font("Audiowide", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnter.Location = new System.Drawing.Point(204, 215);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(91, 48);
+            this.btnEnter.TabIndex = 14;
+            this.btnEnter.Text = "SET";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(323, 299);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.lblFrequencyDisplay);
+            this.Controls.Add(this.btn1);
+            this.Name = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label lblFrequencyDisplay;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnEnter;
+    }
+}
+
---
+++ RadioFrequencyController/MainForm.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.FlightSimulator.SimConnect;
+
+namespace RadioFrequencyController
+{
+    
+    public partial class MainForm : Form
+    {
+        private string frequencyBuffer = "";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn == null || frequencyBuffer.Length >= 6) return; // Ensure max 6 digits
+
+            string newBuffer = frequencyBuffer + btn.Text;
+
+            // If the buffer is empty (after a reset), enforce that first input must be '1'
+            if (frequencyBuffer.Length == 0 && btn.Text != "1")
+            {
+                MessageBox.Show("Frequency must start with '1' (e.g., 118-136).");
+                return;
+            }
+
+            // Validate the prefix before allowing input
+            if (IsValidPrefix(newBuffer))
+            {
+                frequencyBuffer = newBuffer;
+                UpdateDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Invalid frequency prefix! Valid ranges: 118.000 - 136.975 MHz.");
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn == null || frequencyBuffer.Length >= 6) return; // Ensure max 6 digits
+
+            string newBuffer = frequencyBuffer + btn.Text;
+
+            // If the buffer is empty (after a reset), enforce that first input must be '1'
+            if (frequencyBuffer.Length == 0 && btn.Text != "1")
+            {
+                MessageBox.Show("Frequency must start with '1' (e.g., 118-136).");
+                return;
+            }
+
+            // Validate the prefix before allowing input
+            if (IsValidPrefix(newBuffer))
+            {
+                frequencyBuffer = newBuffer;
+                UpdateDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Invalid frequency prefix! Valid ranges: 118.000 - 136.975 MHz.");
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn == null || frequencyBuffer.Length >= 6) return; // Ensure max 6 digits
+
+            string newBuffer = frequencyBuffer + btn.Text;
+
+            // If the buffer is empty (after a reset), enforce that first input must be '1'
+            if (frequencyBuffer.Length == 0 && btn.Text != "1")
+            {
+                MessageBox.Show("Frequency must start with '1' (e.g., 118-136).");
+                return;
+            }
+
+            // Validate the prefix before allowing input
+            if (IsValidPrefix(newBuffer))
+            {
+                frequencyBuffer = newBuffer;
+                UpdateDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Invalid frequency prefix! Valid ranges: 118.000 - 136.975 MHz.");
+            }
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn == null || frequencyBuffer.Length >= 6) return; // Ensure max 6 digits
+
+            string newBuffer = frequencyBuffer + btn.Text;
+
+            // If the buffer is empty (after a reset), enforce that first input must be '1'
+            if (frequencyBuffer.Length == 0 && btn.Text != "1")
+            {
+                MessageBox.Show("Frequency must start with '1' (e.g., 118-136).");
+                return;
+            }
+
+            // Validate the prefix before allowing input
+            if (IsValidPrefix(newBuffer))
+            {
+                frequencyBuffer = newBuffer;
+                UpdateDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Invalid frequency prefix! Valid ranges: 118.000 - 136.975 MHz.");
+            }
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn == null || frequencyBuffer.Length >= 6) return; // Ensure max 6 digits
+
+            string newBuffer = frequencyBuffer + btn.Text;
+
+            // If the buffer is empty (after a reset), enforce that first input must be '1'
+            if (frequencyBuffer.Length == 0 && btn.Text != "1")
+            {
+                MessageBox.Show("Frequency must start with '1' (e.g., 118-136).");
+                return;
+            }
+
+            // Validate the prefix before allowing input
+            if (IsValidPrefix(newBuffer))
+            {
+                frequencyBuffer = newBuffer;
+                UpdateDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Invalid frequency prefix! Valid ranges: 118.000 - 136.975 MHz.");
+            }
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn == null || frequencyBuffer.Length >= 6) return; // Ensure max 6 digits
+
+            string newBuffer = frequencyBuffer + btn.Text;
+
+            // If the buffer is empty (after a reset), enforce that first input must be '1'
+            if (frequencyBuffer.Length == 0 && btn.Text != "1")
+            {
+                MessageBox.Show("Frequency must start with '1' (e.g., 118-136).");
+                return;
+            }
+
+            // Validate the prefix before allowing input
+            if (IsValidPrefix(newBuffer))
+            {
+                frequencyBuffer = newBuffer;
+                UpdateDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Invalid frequency prefix! Valid ranges: 118.000 - 136.975 MHz.");
+            }
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn == null || frequencyBuffer.Length >= 6) return; // Ensure max 6 digits
+
+            string newBuffer = frequencyBuffer + btn.Text;
+
+            // If the buffer is empty (after a reset), enforce that first input must be '1'
+            if (frequencyBuffer.Length == 0 && btn.Text != "1")
+            {
+                MessageBox.Show("Frequency must start with '1' (e.g., 118-136).");
+                return;
+            }
+
+            // Validate the prefix before allowing input
+            if (IsValidPrefix(newBuffer))
+            {
+                frequencyBuffer = newBuffer;
+                UpdateDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Invalid frequency prefix! Valid ranges: 118.000 - 136.975 MHz.");
+            }
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn == null || frequencyBuffer.Length >= 6) return; // Ensure max 6 digits
+
+            string newBuffer = frequencyBuffer + btn.Text;
+
+            // If the buffer is empty (after a reset), enforce that first input must be '1'
+            if (frequencyBuffer.Length == 0 && btn.Text != "1")
+            {
+                MessageBox.Show("Frequency must start with '1' (e.g., 118-136).");
+                return;
+            }
+
+            // Validate the prefix before allowing input
+            if (IsValidPrefix(newBuffer))
+            {
+                frequencyBuffer = newBuffer;
+                UpdateDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Invalid frequency prefix! Valid ranges: 118.000 - 136.975 MHz.");
+            }
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn == null || frequencyBuffer.Length >= 6) return; // Ensure max 6 digits
+
+            string newBuffer = frequencyBuffer + btn.Text;
+
+            // If the buffer is empty (after a reset), enforce that first input must be '1'
+            if (frequencyBuffer.Length == 0 && btn.Text != "1")
+            {
+                MessageBox.Show("Frequency must start with '1' (e.g., 118-136).");
+                return;
+            }
+
+            // Validate the prefix before allowing input
+            if (IsValidPrefix(newBuffer))
+            {
+                frequencyBuffer = newBuffer;
+                UpdateDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Invalid frequency prefix! Valid ranges: 118.000 - 136.975 MHz.");
+            }
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn == null || frequencyBuffer.Length >= 6) return; // Ensure max 6 digits
+
+            string newBuffer = frequencyBuffer + btn.Text;
+
+            // If the buffer is empty (after a reset), enforce that first input must be '1'
+            if (frequencyBuffer.Length == 0 && btn.Text != "1")
+            {
+                MessageBox.Show("Frequency must start with '1' (e.g., 118-136).");
+                return;
+            }
+
+            // Validate the prefix before allowing input
+            if (IsValidPrefix(newBuffer))
+            {
+                frequencyBuffer = newBuffer;
+                UpdateDisplay();
+            }
+            else
+            {
+                MessageBox.Show("Invalid frequency prefix! Valid ranges: 118.000 - 136.975 MHz.");
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            frequencyBuffer = "";  // Clear the input completely
+            lblFrequencyDisplay.Text = "118.000";  // Show a default valid frequency
+        }
+
+        private void UpdateDisplay()
+        {
+            // If the buffer is empty, show a default valid frequency
+            if (string.IsNullOrEmpty(frequencyBuffer))
+            {
+                lblFrequencyDisplay.Text = "118.000";
+                return;
+            }
+
+            // Ensure at least 3 digits before the decimal
+            string formattedFrequency = frequencyBuffer.PadRight(6, '0');
+
+            // Insert the decimal point automatically
+            string displayFrequency = formattedFrequency.Insert(3, ".");
+
+            lblFrequencyDisplay.Text = displayFrequency;
+        }
+
+        private bool IsValidPrefix(string input)
+        {
+            if (input.Length == 1)
+            {
+                // First digit must always be '1'
+                return input == "1";
+            }
+            else if (input.Length == 2)
+            {
+                // Second digit must be 1, 2, or 3 (to allow 118-136 range)
+                char secondDigit = input[1];
+                return secondDigit == '1' || secondDigit == '2' || secondDigit == '3';
+            }
+            else if (input.Length == 3)
+            {
+                // Third digit depends on the second digit:
+                char secondDigit = input[1];
+                char thirdDigit = input[2];
+
+                if (secondDigit == '1') return thirdDigit == '8' || thirdDigit == '9'; // 118, 119
+                if (secondDigit == '2') return thirdDigit >= '0' && thirdDigit <= '9'; // 120-129
+                if (secondDigit == '3') return thirdDigit >= '0' && thirdDigit <= '6'; // 130-136
+            }
+             
+            return false;
+        }
+    }
+}
